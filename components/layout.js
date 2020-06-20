76f6bb93afe724d616162725850ba75dc2af3aa9
Mike Cao <mike@mikecao.com>
2020-06-20 00:40:17 -0700

    Fixed Google analytics include.

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Head from 'next/head';
 import Header from './header';
 import Footer from './footer';
-import Google from './google';
 
 export default function Layout({ title, children }) {
   return (
@@ -14,7 +13,11 @@ export default function Layout({ title, children }) {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
           rel="stylesheet"
         />
-        <Google />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T843VDV');`,
+          }}
+        />
       </Head>
       <Header />
       <main className="container">{children}</main>
