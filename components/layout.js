2e3282332a6612f2af1548bb1244d95aab42782d
Mike Cao <mike@mikecao.com>
2020-07-18 12:25:22 -0700

    Replace google analytics.

--- components/layout.js
+++ components/layout.js
@@ -13,11 +13,14 @@ export default function Layout({ title, children }) {
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap"
           rel="stylesheet"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T843VDV');`,
-          }}
-        />
+        {typeof window !== 'undefined' && (
+          <script
+            async
+            defer
+            data-website-id="7f5a82ce-79e7-452d-b30b-5b65790ac62a"
+            src="https://umami.is/umami.js"
+          />
+        )}
       </Head>
       <Header />
       <main className="container">{children}</main>
