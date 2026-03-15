741596e622df5e16870000a4734c35c418d8f1e2
Mike Cao <mike@mikecao.com>
2026-03-15 05:16:41 -0700

    New website.

--- docs/save-image.md
+++
@@ -1,4 +0,0 @@
-## Save image
-
-To save a snapshot of the stage as an image, click on **Menu > File > Save Image** and select the 
-destination to save your file. This will create a `.png` file.
--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import 'styles/index.css';
-import 'styles/bootstrap-grid.css';
-
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
